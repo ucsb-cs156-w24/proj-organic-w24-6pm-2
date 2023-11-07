66bf0979a416338420ff62c5bbedcf029a5c6df5
Phillip Conrad <pconrad@cs.ucsb.edu>
2023-11-07 13:20:56 -0800

    pc - bring back fixtures needed from happycows

---
+++ frontend/src/fixtures/systemInfoFixtures.js
@@ -0,0 +1,18 @@
+const systemInfoFixtures = {
+    showingAll:
+    {
+        "springH2ConsoleEnabled": true,
+        "showSwaggerUILink": true,
+        "sourceRepo": "https://github.com/ucsb-cs156/proj-happycows"
+    },
+    showingNeither:
+    {
+        "springH2ConsoleEnabled": false,
+        "showSwaggerUILink": false,
+        "sourceRepo": ""
+    }
+};
+
+
+
+export { systemInfoFixtures };

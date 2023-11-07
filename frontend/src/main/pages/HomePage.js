66b773189bbaab4bc649a1df4dc02dcb10460fc6
Phill Conrad <phtcon@ucsb.edu>
2023-11-07 15:48:27 -0800

    pc - fix mutation gap on HomePage

--- frontend/src/main/pages/HomePage.js
+++ frontend/src/main/pages/HomePage.js
@@ -2,9 +2,8 @@ import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { useCurrentUser } from "main/utils/currentUser";
 
 export default function HomePage() {
-  // Stryker disable next-line all: it is acceptable to exclude useState calls from mutation testing
+  // Stryker disable all: it is acceptable to exclude useState calls from mutation testing
   const { data: currentUser } = useCurrentUser();
-  const firstName = (currentUser?.root?.user?.givenName) || "";
   // Stryker restore all
 
   
@@ -12,7 +11,7 @@ export default function HomePage() {
   return (
     <div data-testid={"HomePage-main-div"}>
       <BasicLayout>
-        <h1 data-testid="homePage-title" style={{ fontSize: "75px", borderRadius: "7px", backgroundColor: "white", opacity: ".9" }} className="text-center border-0 my-3">Welcome {firstName}</h1>
+        <h1 data-testid="homePage-title" style={{ fontSize: "75px", borderRadius: "7px", backgroundColor: "white", opacity: ".9" }} className="text-center border-0 my-3">Welcome</h1>
       </BasicLayout>
     </div>
   )

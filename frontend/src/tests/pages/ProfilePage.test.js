910030b1bc889aa89e83f7ddbf443b3391dd5d20
Phillip Conrad <pconrad@cs.ucsb.edu>
2023-11-09 18:40:20 -0800

    pc - fix stryker mutation errors

--- frontend/src/tests/pages/ProfilePage.test.js
+++ frontend/src/tests/pages/ProfilePage.test.js
@@ -1,23 +1,32 @@
-import { render } from "@testing-library/react";
+import { render, waitFor, screen } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
 import ProfilePage from "main/pages/ProfilePage";
-import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 
 describe("ProfilePage tests", () => {
     const queryClient = new QueryClient();
     const axiosMock = new AxiosMockAdapter(axios);
 
-    beforeEach(()=>{
+    beforeEach(() => {
         axiosMock.reset();
         axiosMock.resetHistory();
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+
+        jest.spyOn(console, 'error')
+        // @ts-ignore jest.spyOn adds this functionallity
+        console.error.mockImplementation(() => null);
     });
+
+    afterEach(() => {
+        // @ts-ignore jest.spyOn adds this functionallity
+        console.error.mockRestore()
+    })
 
     test("renders correctly for regular logged in user", async () => {
         render(
@@ -27,8 +36,6 @@ describe("ProfilePage tests", () => {
                 </MemoryRouter>
             </QueryClientProvider>
         );
-
-      
     });
 
     test("renders correctly for admin user from UCSB", async () => {
@@ -39,8 +46,23 @@ describe("ProfilePage tests", () => {
                 <MemoryRouter>
                     <ProfilePage />
                 </MemoryRouter>
-            </QueryClientProvider>  
+            </QueryClientProvider>
         );
+
+    });
+
+    test("renders correct content when not logged in", async () => {
+        axiosMock.onGet("/api/currentUser").reply(403);
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <ProfilePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => expect(screen.getByText("Not logged in.")).toBeInTheDocument());
 
     });
 });

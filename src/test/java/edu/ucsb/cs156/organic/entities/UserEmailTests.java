6a8030054dbd29c86637420c919cde792ce23f01
Phillip Conrad <pconrad@cs.ucsb.edu>
2023-11-09 16:38:59 -0800

    pc - add trivial tests for User and UserEmail to avoid red on pitest report

--- src/test/java/edu/ucsb/cs156/organic/entities/UserEmailTests.java
+++ src/test/java/edu/ucsb/cs156/organic/entities/UserEmailTests.java
@@ -5,6 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class UserEmailTests {
+
+    @Test void test_basic_getters() {
+        User user = User.builder().githubId(12345).githubLogin("cgaucho").build();
+        UserEmail userEmail = UserEmail.builder().email("cgaucho@ucsb.edu").user(user).build();
+        assertEquals("cgaucho@ucsb.edu", userEmail.getEmail());
+        assertEquals(user, userEmail.getUser());
+    }
+
     @Test
     void test_getGithubId() {
         User user = User.builder().githubId(12345).githubLogin("cgaucho").build();
--- src/test/java/edu/ucsb/cs156/organic/entities/UserTests.java
+++ src/test/java/edu/ucsb/cs156/organic/entities/UserTests.java
@@ -5,6 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class UserTests {
+
+    @Test
+    void test_noArgConstructor() {
+        User user = new User();
+        assertEquals(null, user.getGithubId());
+        assertEquals(null, user.getGithubLogin());
+    }
+
     @Test
     void test_toString() {
         User user = User.builder().githubId(12345).githubLogin("cgaucho").build();

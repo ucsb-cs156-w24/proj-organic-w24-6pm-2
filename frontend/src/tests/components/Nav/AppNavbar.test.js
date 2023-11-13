da202504c3a7f643156c2cbe242abef4deb5feac
Phillip Conrad <pconrad@cs.ucsb.edu>
2023-11-12 18:56:16 -0800

    pc - fix failing test and broken code for showing github login in welcome link to profile page

--- frontend/src/tests/components/Nav/AppNavbar.test.js
+++ frontend/src/tests/components/Nav/AppNavbar.test.js
@@ -21,7 +21,7 @@ describe("AppNavbar tests", () => {
             </QueryClientProvider>
         );
 
-        expect(await screen.findByText("Welcome")).toBeInTheDocument();
+        expect(await screen.findByText("Welcome, cgaucho")).toBeInTheDocument();
     });
 
     test("renders correctly for admin user", async () => {
@@ -36,7 +36,7 @@ describe("AppNavbar tests", () => {
             </QueryClientProvider>
         );
 
-        expect(await screen.findByText("Welcome")).toBeInTheDocument();
+        expect(await screen.findByText("Welcome, pconrad")).toBeInTheDocument();
         const adminMenu = screen.getByTestId("appnavbar-admin-dropdown");
         expect(adminMenu).toBeInTheDocument();        
     });

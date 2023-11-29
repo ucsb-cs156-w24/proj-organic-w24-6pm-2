464267a243f1aa78ddbf259564ce7393d2eed290
Daryl Ou <darylou@ucsb.edu>
2023-11-29 15:15:31 -0800

    do - #13 - useBackend rather than users

--- frontend/src/main/pages/AdminUsersPage.js
+++ frontend/src/main/pages/AdminUsersPage.js
@@ -1,11 +1,19 @@
 import React from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import UsersTable from "main/components/Users/UsersTable"
+import { useBackend } from "main/utils/useBackend";
 
-import { useUsers } from "main/utils/users";
 const AdminUsersPage = () => {
 
-    const { data: users } = useUsers();
+    const { data: users, error: _error, status: _status } =
+        useBackend(
+            // Stryker disable next-line all : don't test internal caching of React Query
+            ["/api/admin/users"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+            { method: "GET", url: "/api/admin/users" },
+            // Stryker disable next-line all
+            []
+        )
 
     return (
         <BasicLayout>

010e58abd7c17354c5990e0e906657313c8b44bd
Demir <demirince@ucsb.edu>
2024-02-27 18:27:10 -0800

    done

--- frontend/src/main/pages/AdminUsersPage.js
+++ frontend/src/main/pages/AdminUsersPage.js
@@ -5,6 +5,7 @@ import { useBackend } from "main/utils/useBackend";
 
 const AdminUsersPage = () => {
 
+
     const { data: users, error: _error, status: _status } =
         useBackend(
             // Stryker disable next-line all : don't test internal caching of React Query

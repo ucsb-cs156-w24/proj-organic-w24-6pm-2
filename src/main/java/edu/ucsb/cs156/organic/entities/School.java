658349eb2c7ad95692fec94e6b29a863635115ac
Mihir Bharti <mihirbharti@Mihirs-Laptop.local>
2024-03-06 18:48:56 -0800

    for initial PR

---
+++ src/main/java/edu/ucsb/cs156/organic/entities/School.java
@@ -0,0 +1,22 @@
+package edu.ucsb.cs156.organic.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Entity(name = "schools")
+public class School {
+    // is the @Id necessary?
+  @Id
+  private String abbreviation;
+  private String name;
+  private String termRegex;
+  private String termDescription;
+  private String termError;
+}

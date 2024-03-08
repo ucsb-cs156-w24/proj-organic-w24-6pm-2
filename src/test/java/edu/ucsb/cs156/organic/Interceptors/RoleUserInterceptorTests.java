ff472396ad06edc6db2876dc16998283f846c9b5
Vincent Zhu <vincentzhu@ucsb.edu>
2024-03-07 17:01:07 -0800

    Reverted the changes to .interceptor

--- src/test/java/edu/ucsb/cs156/organic/Interceptors/RoleUserInterceptorTests.java
+++ src/test/java/edu/ucsb/cs156/organic/Interceptors/RoleUserInterceptorTests.java
@@ -1,4 +1,4 @@
-package edu.ucsb.cs156.organic.Interceptors;
+package edu.ucsb.cs156.organic.interceptors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import edu.ucsb.cs156.organic.entities.User;
+import edu.ucsb.cs156.organic.interceptors.RoleUserInterceptor;
 import edu.ucsb.cs156.organic.controllers.ControllerTestCase;
 import edu.ucsb.cs156.organic.repositories.UserRepository;
 import org.springframework.boot.test.context.SpringBootTest;

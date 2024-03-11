d423a057ac42499e26f328166f595a7cf0f4ad1c
Vincent Zhu <vincentzhu@ucsb.edu>
2024-03-11 15:46:18 -0700

    These imports are never used

--- src/main/java/edu/ucsb/cs156/organic/Interceptors/RoleUserInterceptor.java
+++ src/main/java/edu/ucsb/cs156/organic/Interceptors/RoleUserInterceptor.java
@@ -6,12 +6,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
+// import org.springframework.web.servlet.ModelAndView;
 
 import edu.ucsb.cs156.organic.repositories.UserRepository;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;

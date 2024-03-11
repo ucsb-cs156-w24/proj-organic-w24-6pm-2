2f262aadb18452554b11843a8d63720dde382e8b
Vincent Zhu <vincentzhu@ucsb.edu>
2024-03-11 16:03:39 -0700

    Removed unused imports

--- src/main/java/edu/ucsb/cs156/organic/Interceptors/RoleUserInterceptorAppConfig.java
+++ src/main/java/edu/ucsb/cs156/organic/Interceptors/RoleUserInterceptorAppConfig.java
@@ -1,7 +1,6 @@
 package edu.ucsb.cs156.organic.interceptors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
